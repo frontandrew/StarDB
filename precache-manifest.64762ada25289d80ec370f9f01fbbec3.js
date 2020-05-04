3904e6b3e4bf5daacb6e91760a73f8a8ddddd0a0
Andrew Bubnov <jackblack.nt@ya.ru>
2020-05-04 22:13:45 +1100

    Updates

---
+++ precache-manifest.64762ada25289d80ec370f9f01fbbec3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1cbce9d4e6e805d1db5e0a83bac1e09a",
+    "url": "/star-db/index.html"
+  },
+  {
+    "revision": "27b2d2e03ae824ba3878",
+    "url": "/star-db/static/css/main.84dc6e2a.chunk.css"
+  },
+  {
+    "revision": "e67d932be1a00a091870",
+    "url": "/star-db/static/js/2.411eadf9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/star-db/static/js/2.411eadf9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "27b2d2e03ae824ba3878",
+    "url": "/star-db/static/js/main.c823fd23.chunk.js"
+  },
+  {
+    "revision": "cb942bc5f7672a7c25c5",
+    "url": "/star-db/static/js/runtime-main.24cb81dd.js"
+  }
+]);
