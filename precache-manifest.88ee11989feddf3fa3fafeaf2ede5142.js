8e2ecdd76c2c58ba5df6542ad1a65d8c92b5bbc2
Andrew Bubnov <jackblack.nt@ya.ru>
2020-05-22 22:02:35 +1100

    Updates

---
+++ precache-manifest.88ee11989feddf3fa3fafeaf2ede5142.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "393bde96ae6a1eecfd28a8f82db4aec6",
+    "url": "/star-db/index.html"
+  },
+  {
+    "revision": "d46d4514a07d5cb88abe",
+    "url": "/star-db/static/css/main.19ab434c.chunk.css"
+  },
+  {
+    "revision": "16f5cfc6498579e5fec6",
+    "url": "/star-db/static/js/2.ea557be7.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/star-db/static/js/2.ea557be7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d46d4514a07d5cb88abe",
+    "url": "/star-db/static/js/main.2fbb2f18.chunk.js"
+  },
+  {
+    "revision": "cb942bc5f7672a7c25c5",
+    "url": "/star-db/static/js/runtime-main.24cb81dd.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/star-db/precache-manifest.c9c16e66cac8b191e10cbf0750bf5466.js"
+  "/star-db/precache-manifest.88ee11989feddf3fa3fafeaf2ede5142.js"
 );
 
 self.addEventListener('message', (event) => {
